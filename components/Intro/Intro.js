8b56446df54fd6352522d9d6883d9ea4344ad858
sohalkumar <sohalkumarsingh@email.com>
2024-05-31 17:23:25 +0530

    fixed layout issues on mobile screens

--- components/Intro/Intro.js
+++ components/Intro/Intro.js
@@ -8,7 +8,7 @@ export default function Intro() {
       transition={{ duration: 0.3 }}
       className="bg-micro-6-intro bg-cover bg-no-repeat lg:bg-cover px-4 py-[2.75rem] lg:px-lg-x lg:py-[16rem] w-screen"
     >
-      <div className="text-center w-[45%]">
+      <div className="text-start lg:text-center w-fit lg:w-[50%]">
         <h3 className="font-caladea font-bold text-[25px] lg:text-[49px] tracking-[-0.5px]">
           Introducing
         </h3>
@@ -17,8 +17,9 @@ export default function Intro() {
         </h2>
         <p className="lg:text-[29px]">
           An <span className="text-primaryPurple">affordable</span> mini PC
-          which delivers{" "}
-          <span className="text-primaryPurple">high performance</span> even on{" "}
+          which <br className="lg:hidden" /> delivers{" "}
+          <span className="text-primaryPurple">high performance</span> even on
+          <br className="lg:hhidden" />
           <span className="text-primaryPurple">low power</span>
         </p>
       </div>
